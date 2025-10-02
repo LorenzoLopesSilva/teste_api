ee2ae13ee03743e2dec82c6c27c66a0b08dfba54
LorenzoLopesSilva <lorenzo.lopes0808@gmail.com>
2025-10-02 16:53:57 -0300

    16:53

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.add_middleware(
 @app.post("/items/")
 async def criar_item(item: Item):
 
-    return{"mensagem": f"Olá {Item.nome}"}
+    return{"mensagem": f"Olá {item.nome}"}
 
     # return JSONResponse (
     #     content={"mensagem": f"Seja bem vindo {nome}"}
