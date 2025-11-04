0d8b6a922b0cd31521ece5714fcf211c64177a50
LorenzoLopesSilva <lorenzo.lopes0808@gmail.com>
2025-11-04 12:51:36 -0300

    Teste - 04-11-25 12:51

--- main.py
+++ main.py
@@ -9,33 +9,9 @@ class Pessoas(BaseModel):
 
 app = FastAPI()
 
-vendas = {
-    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
-    2: {"item": "garrafa", "preco_unitario": 9, "quantidade": 5},
-    3: {"item": "galão", "preco_unitario": 16, "quantidade": 5},
-    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5},
-
-}
-
 @app.get("/")
 def home():
-    return {"Vendas": len(vendas)}
-
-@app.get("/vendas/{id_venda}")
-def pegar_venda(id_venda: int):
-    if id_venda in vendas:
-        return vendas[id_venda]
-    else:
-        return {"Erro": "ID Venda Inexistente"}
-
-
-@app.get("/teste/")
-def teste():
-    return "Odinospitos"
-
-@app.post("/teste-post/")
-def create_item(pessoa: Pessoas):
-    return pessoa.nome
+    return "Acessar /pessoas"
 
 #----------------------------------------------------------
 df = pd.read_excel('teste.xlsx', sheet_name='Planilha')
