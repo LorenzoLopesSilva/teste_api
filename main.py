6b994a961578a4713ee268bead79dbbd0ac82562
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-11-04 08:42:21 -0300

    Teste - 04-11-25 08:42

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+vendas = {
+    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+    2: {"item": "garrafa", "preco_unitario": 9, "quantidade": 5},
+    3: {"item": "galão", "preco_unitario": 16, "quantidade": 5},
+    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5},
+
+}
+
 @app.get("/")
 def home():
-    return "Minha api está no ar"
+    return {"Vendas": len(vendas)}
 
 @app.get("/teste/")
 def teste():
