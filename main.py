50995e911b63bad77e48001e37e83d83719fd19a
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-11-04 08:33:13 -0300

    Teste - 04-11-25 08:33

--- main.py
+++ main.py
@@ -1,42 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import JSONResponse
-
-class Item(BaseModel):
-    nome: str
-    email: str
-
-#
 
 app = FastAPI()
 
-
-origins = [
-    "*"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-@app.post("/items/")
-async def criar_item(item: Item):
-
-    # return{"mensagem": f"Olá {item.nome}"}
-
-    return JSONResponse (
-        content={"mensagem": f"Seja bem vindo {item.nome}"}
-    )
-
 @app.get("/")
-async def read_root():
-    return {"hello": "World"}
-
-@app.get('/teste/')
-async def read_root():
-    return {"Hello": "Nova Branch Ultimate"}
+def home():
+    return "Minha api está no ar"
