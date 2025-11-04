33294533af01dd3499ea37b592153650a0982820
LorenzoLopesSilva <lorenzo.lopes0808@gmail.com>
2025-11-04 16:45:51 -0300

    Up

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 def root():
     return FileResponse("index.html")
 
-@app.post("/postdata")
+@app.post("/postdata/")
 def postdata(username: str = Form(), userage: int = Form()):
     return {"name": username, "age": userage}
